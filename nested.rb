f14d882b615f841d291384ce8e62e40decb8ce7e
Sharnell Grant <sharnellgrant@gmail.com>
2019-12-07 03:22:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,8 +81,9 @@ def adding_matz
         }
      }
 
-    programmer_hash[:]
+    programmer_hash[:yukihiro_matsumoto][:known_for][:languages]
     programmer_hash
+    programmer_hash[:known_for] = "Ruby"
 end
 
 def changing_alan
